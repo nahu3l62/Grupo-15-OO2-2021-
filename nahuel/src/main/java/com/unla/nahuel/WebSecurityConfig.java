8df8dd2526beb8937ff109209bd63b8fed9f3faa
Roger Ricciardulli <roger01010101@hotmail.com>
2021-05-24 18:26:37 -0300

    Agregando autenticacion de rol

--- nahuel/src/main/java/com/unla/nahuel/WebSecurityConfig.java
+++ nahuel/src/main/java/com/unla/nahuel/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -26,4 +27,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		.authoritiesByUsernameQuery("SELECT u.nombre_de_usuario, p.rol from perfiles p inner join usuario u on p.id=u.perfiles_id where u.nombre_de_usuario=?");
 
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/perfiles/").hasAuthority("Administrador")
+		.antMatchers("/perfiles/lista").permitAll()
+		.antMatchers("/perfiles/lista/edit/**").hasAuthority("Administrador")
+		.antMatchers("/perfiles/lista/delete/**").hasAuthority("Administrador")
+		.antMatchers("/usuarios/").hasAuthority("Administrador")
+		.antMatchers("/usuarios/lista").permitAll()
+		.antMatchers("/usuarios/lista/edit/**").hasAuthority("Administrador")
+		.antMatchers("/usuarios/lista/delete/**").hasAuthority("Administrador")
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().permitAll()
+		.and()
+		.logout().permitAll();
+	}
+	
 }
