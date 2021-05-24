31af5ee8ec24b1eb036cffd3f5b2ea3084815971
Roger Ricciardulli <roger01010101@hotmail.com>
2021-05-24 20:24:33 -0300

    Commit

--- nahuel/src/main/java/com/unla/nahuel/controllers/UsuarioController.java
+++ nahuel/src/main/java/com/unla/nahuel/controllers/UsuarioController.java
@@ -3,7 +3,6 @@ package com.unla.nahuel.controllers;
 import java.util.ArrayList;
 
 import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
