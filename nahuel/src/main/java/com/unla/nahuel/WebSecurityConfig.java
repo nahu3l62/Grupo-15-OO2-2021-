76a61723ffcd636687fc2f50a6c598d14c7e2195
Martin <Martin@DESKTOP-9BFTLM9>
2021-06-03 16:05:19 -0300

    Accesos como pdf

--- nahuel/src/main/java/com/unla/nahuel/WebSecurityConfig.java
+++ nahuel/src/main/java/com/unla/nahuel/WebSecurityConfig.java
@@ -32,8 +32,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers("/index","/","/css/**","/images/**","/js/**","/vendor/**").permitAll()
-		.antMatchers("/permiso/").permitAll()//traer permiso por persona
-		.antMatchers("/permiso/seleccionarDni").permitAll()//traer permiso por persona
+		.antMatchers("/permiso/**").access("anonymous or hasAuthority('Auditor')")//traer permiso por persona
+		.antMatchers("/permiso/seleccionarDni").access("anonymous or hasAuthority('Auditor')")//traer permiso por persona
 		.antMatchers("/permiso/lugar").hasAuthority("Auditor")//traer permiso por lugar
 		.antMatchers("/permiso/periodoXlugar/**").hasAuthority("Auditor")//traer permiso por lugar(diario)
 		.antMatchers("/permiso/diarioXlugar/**").hasAuthority("Auditor")//traer permiso por lugar(periodo)
@@ -42,26 +42,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		.antMatchers("/permiso/activoDiario").hasAuthority("Auditor")//traer permiso activo
 		.antMatchers("/permiso/activoFechaLugarDiario").hasAuthority("Auditor")//traer permiso activo
 		.antMatchers("/permiso/activoFechaLugarPeriodo").hasAuthority("Auditor")//traer permiso activo
-		.antMatchers("/permiso/traerFechaLugarDiario").hasAuthority("Auditor")//traer permiso activo
 		.antMatchers("/permiso/traerFechaLugarPeriodo").hasAuthority("Auditor")//traer permiso activo
+		.antMatchers("/permiso/traerFechaLugarDiario").hasAuthority("Auditor")//traer permiso activo
 		.antMatchers("/permiso/fechaDiario").hasAuthority("Auditor")//traer permiso activo(periodo)
-		.antMatchers("/permiso/lista/permisoDiario/**").permitAll()//traer permiso por persona(diario)
-		.antMatchers("/permiso/lista/permisoPeriodo/**").permitAll()//traer permiso por persona(periodo)
-		.antMatchers("/permiso_diario/").permitAll()
+		.antMatchers("/permiso/lista/permisoDiario/**").access("anonymous or hasAuthority('Auditor')")//traer permiso por persona(diario)
+		.antMatchers("/permiso/lista/permisoPeriodo/**").access("anonymous or hasAuthority('Auditor')")//traer permiso por persona(periodo)
+		.antMatchers("/permiso_diario/").anonymous()
+		.antMatchers("/permiso_diario/**").anonymous()
 		.antMatchers("/permiso_diario/lista").permitAll()
-		.antMatchers("/permiso_diario/seleccionarDni").permitAll()
-		.antMatchers("/permiso_periodo/").permitAll()
+		.antMatchers("/permiso_diario/seleccionarDni").anonymous()
+		.antMatchers("/permiso_periodo/").anonymous()
+		.antMatchers("/permiso_periodo/**").anonymous()
 		.antMatchers("/permiso_periodo/lista").permitAll()
-		.antMatchers("/permiso_periodo/seleccionarDni").permitAll()
+		.antMatchers("/permiso_periodo/seleccionarDni").anonymous()
 		.antMatchers("/permiso_periodo/rodados/**").hasAuthority("Auditor")
-		.antMatchers("/persona/").permitAll()
+		.antMatchers("/persona/").anonymous()
 		.antMatchers("/perfiles/").hasAuthority("Administrador")
 		.antMatchers("/perfiles/lista").hasAnyAuthority("Auditor", "Administrador")
 		.antMatchers("/perfiles/lista/edit/**").hasAuthority("Administrador")
 		.antMatchers("/perfiles/lista/delete/**").hasAuthority("Administrador")
 		.antMatchers("/perfiles/lista").hasAnyAuthority("Auditor", "Administrador")
 		.antMatchers("/perfiles/lista/**").hasAnyAuthority("Auditor", "Administrador")//pdf
-		.antMatchers("/rodado/").permitAll()
+		.antMatchers("/rodado/").anonymous()
 		.antMatchers("/rodado/seleccionarDominio").hasAuthority("Auditor")
 		.antMatchers("/rodado/dominio").hasAuthority("Auditor")
 		.antMatchers("/rodado/lista").hasAuthority("Auditor")
