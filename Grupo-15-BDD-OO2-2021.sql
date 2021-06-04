22686635a8749c53d5b9b4c64e6557b6e13403ca
nahu3l62 <82968302+nahu3l62@users.noreply.github.com>
2021-06-04 19:45:58 -0300

    Delete Grupo-15-BDD-OO2-2021.sql

--- Grupo-15-BDD-OO2-2021.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE  IF NOT EXISTS `Grupo-15-BDD-OO2-2021`;
-USE `Grupo-15-BDD-OO2-2021`;
-
-INSERT INTO perfiles VALUES(1,true,'Administrador');
-INSERT INTO perfiles VALUES(2,true,'Auditor');
-INSERT INTO usuario VALUES(1,'User','$2a$10$SUJ4cNh2BsoR.i9KQxfnzOc8csk50wrBhX8tX.qid.Ogbz.KwUrSK','user@hotmail.com',true,0,'User','user','D.N.I.',1);
-
-select *from perfiles;
-select *from usuario;
